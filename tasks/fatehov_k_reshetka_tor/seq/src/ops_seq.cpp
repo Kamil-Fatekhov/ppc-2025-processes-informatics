42c59a6f438d03e73da8cfdf435a28546632c95b
Kamil <frister22@yandex.ru>
2026-01-02 13:42:44 +0300

    tidy fixes_4.0

--- tasks/fatehov_k_reshetka_tor/seq/src/ops_seq.cpp
+++ tasks/fatehov_k_reshetka_tor/seq/src/ops_seq.cpp
@@ -1,9 +1,10 @@
 #include "fatehov_k_reshetka_tor/seq/include/ops_seq.hpp"
 
+#include <algorithm>
 #include <cmath>
 #include <complex>
 #include <vector>
-#include <algorithm>
+
 #include "fatehov_k_reshetka_tor/common/include/common.hpp"
 
 namespace fatehov_k_reshetka_tor {
